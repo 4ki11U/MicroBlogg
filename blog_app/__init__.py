3809a8a16b49dc8b7b8abdf0810930e55e084bf6
4killU <kpakhotin@ukr.net>
2022-07-08 21:59:27 +0300

    fisrt deploy

--- blog_app/__init__.py
+++ blog_app/__init__.py
@@ -3,15 +3,37 @@ from flask_login import LoginManager
 from flask import Flask
 from os import path
 
+db = SQLAlchemy()
+DB_NAME = r'database/blog_database.db'
 
 def start_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'helloworld'
 
-    from .views import views
-    from .auth import auth
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
+    from .views import views
     app.register_blueprint(views, url_prefix='/')
+
+    from .auth import auth
     app.register_blueprint(auth, url_prefix='/')
 
+    from .models import User, Post, Comment, Like
+    create_database(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
+
     return app
+
+def create_database(app):
+    if not path.exists('blog_app' + DB_NAME):
+        print('Created DATABASE is OK')
+        db.create_all(app=app)
--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 from blog_app import start_app
 
-
 if __name__ == "__main__":
     app = start_app()
     app.run(debug=True)
